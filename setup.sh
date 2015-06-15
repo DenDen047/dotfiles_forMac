2a2c33b546659cd9bdf7ee2a985e559bde7c5f64
Naoya <naoyashop@yahoo.co.jp>
2015-06-15 21:14:52 +0900

    +install section to setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 !#/bin/zsh
 
+echo "creat Symbolic Link..."
 ln -s ~/dotfiles_forMac/.vimrc ~/.vimrc
 ln -s ~/dotfiles_forMac/.zshenv ~/.zshenv
 
+echo "Installing lib of pip..."
+pip install requests
+pip install BeautifulSoup
 
