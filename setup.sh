714a01fac81dfa28d75d6e1881bbba472dfacee5
Naoya <naoyashop@yahoo.co.jp>
2015-07-16 18:51:04 +0900

    add ln command of .ssh to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ mkdir ~/dotfiles_forMac/.vim/bundle
 echo "creat Symbolic Link..."
 ln -s ~/dotfiles_forMac/.vimrc ~/.vimrc
 ln -s ~/dotfiles_forMac/.zshenv ~/.zshenv
+ln -s ~/dotfiles_forMac/.ssh/config ~/.ssh/config
 
 echo "Installing lib of pip..."
 pip install requests
