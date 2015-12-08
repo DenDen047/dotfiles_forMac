50b3f435dd17ffcde38d3b22f63885ff9d036f6d
sh.mn.nat@gmail.com <naoyashop@yahoo.co.jp>
2015-12-08 21:29:25 +0900

    update test

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 rm -rf ~/dotfiles_forMac/.vim/bundle
 mkdir ~/dotfiles_forMac/.vim/bundle
 
+
 echo "creat Symbolic Link..."
 ln -s ~/dotfiles_forMac/.vimrc ~/.vimrc
 ln -s ~/dotfiles_forMac/.zshenv ~/.zshenv
