2094e28a7455c600c5eddf604b2d94f19ec9ff3a
DenDen047 <sh.mn.nat@gmail.com>
2016-11-30 15:31:20 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,54 @@ ln -s ~/dotfiles_forMac/.zshenv ~/.zshenv
 ln -s ~/dotfiles_forMac/.ssh/config ~/.ssh/config
 ln -s ~/dotfiles_forMac/.theano/.theanorc ~/.theanorc
 
+echo "Installing brew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew tap homebrew/dupes
+brew tap homebrew/science
+brew tap homebrew/versions
+# Other Homebrew packages
+brew install gcc
+brew install git
+brew install rmtrash
+brew install vim
+brew install zsh-completions
+brew install tree
+brew install ghostscript
+brew install imagemagick
+brew install python
+brew install pyenv-virtualenv    # OSX 10.9,10.10 need blow command. ->   CFLAGS="-I$(xcrun --show-sdk-path)/usr/include" pyenv install -v 2.7.  <- want version
+brew install wget
+brew install docker
+brew install docker-machine
+brew install boot2docker
+brew install phantomjs
+# brew-cask packages
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install skype
+brew cask install virtualbox
+brew cask install iterm2
+brew cask install python
+brew cask install sublime-text
+brew cask install transmission
+brew cask install burn
+brew cask install appcleaner
+brew cask install flux
+brew cask install alfred
+brew cask install onyx
+brew cask install ccleaner
+brew cask install r
+brew cask install rstudio
+brew cask install coolterm
+brew cask install cyberduck
+brew cask install the-unarchiver
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cleanup
+brew cask cleanup
+
 echo "Installing lib of pip..."
 pip install --upgrade pip
 pip install ipython
